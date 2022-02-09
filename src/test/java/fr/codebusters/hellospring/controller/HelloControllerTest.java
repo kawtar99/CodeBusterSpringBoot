e878f3e9c0ff3a8e7741ad6e88205a5c84274779
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-09 15:23:16 +0100

    test controller

---
+++ src/test/java/fr/codebusters/hellospring/controller/HelloControllerTest.java
@@ -0,0 +1,69 @@
+package fr.codebusters.hellospring.controller;
+
+import fr.codebusters.hellospring.entity.Greeting;
+import fr.codebusters.hellospring.service.GreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GreetingService service;
+
+    @BeforeEach
+    public void setUp(){
+        Greeting g1 = new Greeting(1L, "first greeting");
+        Greeting g2 = new Greeting(2L, "second greeting");
+
+        when(service.list()).thenReturn(List.of(g1, g2));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Spring Boot!")));
+    }
+
+    @Test
+    void list() throws Exception {
+        this.mockMvc.perform(get("/greetings"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[1].id", is(2)));
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void save() {
+    }
+}

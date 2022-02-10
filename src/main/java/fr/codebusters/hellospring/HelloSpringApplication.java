e8c21a411f7a4a3cabd03d0580766ad8d6c42b25
Kawtar99 <kawtar.tarib@outlook.fr>
2022-02-11 00:59:20 +0100

    use PostConstruct and PreDetroy

--- src/main/java/fr/codebusters/hellospring/HelloSpringApplication.java
+++ src/main/java/fr/codebusters/hellospring/HelloSpringApplication.java
@@ -3,11 +3,24 @@ package fr.codebusters.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+	@PostConstruct
+	public void sayHello(){
+		System.out.println("Welcome to the Greeting application!");
+	}
+
+	@PreDestroy
+	public void sayGoodBye(){
+		System.out.println("The application stopped running. Goodbye!");
 	}
 
 }
